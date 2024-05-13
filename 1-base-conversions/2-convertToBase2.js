905316428e52d81e57bdb2239b42a9f07230722c
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2024-05-13 10:43:21 -0400

    We completed convertTobase2

--- 1-base-conversions/2-convertToBase2.js
+++ 1-base-conversions/2-convertToBase2.js
@@ -4,7 +4,93 @@
 
 const convertToBase2 = element => {
   // Your code here
+
+  /*
+  We will create and empty string
+  We need to test if the element is a integer or string
+  if string
+    We will iterate backwards to excluding 1
+    We will test if the element is an integer
+      if true, we will parse the element and call covertNumToBinary()
+        We will add the result to the front of the empty string
+      if false, we will need to reference hexBinary
+        add the result to the front of the empty string
+  if integer
+    All we need to do is just call covertNumToBinary() and save the result
+
+  last return 0b + String(str);
+
+  We need to shave off the extra 0s
+  */
+
+  let str = '';
+  const hexBinary = {
+    '0': '0000',
+    '1': '0001',
+    '2': '0010',
+    '3': '0011',
+    '4': '0100',
+    '5': '0101',
+    '6': '0110',
+    '7': '0111',
+    '8': '1000',
+    '9': '1001',
+    'a': '1010',
+    'b': '1011',
+    'c': '1100',
+    'd': '1101',
+    'e': '1110',
+    'f': '1111'
+  };
+
+  if (Number.isInteger(element)) {
+
+    return '0b' + covertNumToBinary(element);
+  } else {
+
+    for (let i = element.length - 1; i > 1; i--) {
+      const elem = element[i];
+
+      str = hexBinary[elem] + str;
+    }
+
+  }
+
+  return '0b' + shaveExtraZeros(str);
 };
+
+const shaveExtraZeros = str => {
+  const index = str.indexOf('1');
+  return str.slice(index);
+}
+
+const covertNumToBinary = num => {
+  /*
+  This will recieve integer
+  The goal here is to constantly divide the num
+  We will have and empty string.
+
+    if the number is modulo by 2, we will add 0 to the empty string.
+    if the number is not modulo by 2, we will add 1 to the empty string.
+    However, we want the divided result. We will continue divide it till result is less than 1 which will give us our last 1
+    The numbers will be added to the to the front of the empty string
+*/
+
+  let str = "";
+
+  while(num >= 1) {
+
+    if (num % 2 === 0) {
+      str = '0' + str;
+    } else {
+      str = '1' + str;
+    }
+
+    num = Math.floor(num / 2);
+  }
+
+  return str;
+}
 
 /******************************************************************************/
 
